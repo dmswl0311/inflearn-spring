ffa3fb59c645e68ca6c49edcfd6ea003e109c775
dmswl0311 <eunji.cho2@cj.net>
2024-03-11 17:46:45 +0900

    ~섹션 6. 컴포넌트 스캔

---
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,22 @@
+package hello.core.scan;
+
+import hello.core.member.MemberService;
+import hello.core.AutoAppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AutoAppConfigTest {
+
+    @Test
+    @DisplayName("@Component 애노테이션 테스트")
+    void test() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
